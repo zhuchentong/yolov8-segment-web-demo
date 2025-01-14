f9b77f5782003803f7cff92d0179c481d249eefd
zhuchentong <2037630@163.com>
2025-01-14 15:17:16 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import unocss from 'unocss/vite'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), unocss()],
   optimizeDeps: {
     exclude: [
-      'onnxruntime-web'
-    ]
+      'onnxruntime-web',
+    ],
   },
 })
